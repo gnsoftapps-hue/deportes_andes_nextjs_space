ff2253ee3bab53755431e150fbbc684aec5c5ecb
gnsoftapps-hue <gnsoftapps@gmail.com>
2026-07-28 21:57:17 -0400

    Limpiar nextconfg

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
-const originalConfig = require('./next.config.visual_edit_bak.js');
-const raw = (typeof originalConfig === 'object' && originalConfig.__esModule) ? originalConfig.default : originalConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* Puedes agregar aquí configuraciones si las necesitas más adelante */
+};
 
-function injectWebpack(config) {
-  const orig = config.webpack;
-  config.webpack = (webpackConfig, options) => {
-    webpackConfig.module.rules.push({
-      test: /\.(tsx|jsx)$/,
-      exclude: /node_modules/,
-      enforce: 'pre',
-      use: [{ loader: '/opt/hostedapp/node/root/ast_worker/inspector-loader.js' }],
-    });
-    return orig ? orig(webpackConfig, options) : webpackConfig;
-  };
-  return config;
-}
-
-if (typeof raw === 'function') {
-  module.exports = (...args) => Promise.resolve(raw(...args)).then(c => injectWebpack(c));
-} else {
-  module.exports = injectWebpack({ ...raw });
-}
+module.exports = nextConfig;
